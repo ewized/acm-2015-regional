b874289be832ac67dfb94123ff4fd174c722c35b
Joshua Rodriguez <ewized@gmail.com>
2015-11-19 11:32:50 -0800

    Add problem 08 soultion

---
+++ p08.cpp
@@ -0,0 +1,27 @@
+// Copyright 2015 Joshua Rodriguez
+#include <iostream>
+
+using namespace std;
+
+// Use recursion to return the number of loops it takes
+// and only use a max of 9 loops
+int caculate(int n, int loop=0) {
+    if (n < 10 || loop == 9) {
+        return loop;
+    }
+    int total = 1;
+    while (n > 0) {
+        int a = n % 10;
+        n /= 10;
+        total *= a;
+    }
+    return caculate(total, loop + 1);
+}
+
+int main() {
+    int n;
+    while (cin >> n) {
+        cout << n << " " << caculate(n) << endl;
+    }
+    return 0;
+}
